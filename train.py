c4b98e0ec4f75856af4b26869ca9093e5a8fe581
Akash Desarda <desardaakash@gmail.com>
2020-02-23 16:47:39 +0530

    changes to on board embeddings logic

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ now = datetime.now().strftime("%d-%m-%Y")
 from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
-import tensorflow_hub as hub
+# import tensorflow_hub as hub
 from keras import backend as K
 from keras.utils import to_categorical
 
@@ -22,18 +22,20 @@ from callbacks import callbacks
 import mlflow
 
 data = pd.read_csv('./data/train_que.csv')
-print("[INFO]...Data loaded Successfully")
-data.dropna(inplace = True)
-data['question1'], data['question2'] = data['question1'].apply(clean_text), data['question2'].apply(clean_text)
-print("[INFO]...Data transformation completed")
+data.dropna(inplace=True)
+# print("[INFO]...Data loaded Successfully")
+# data.dropna(inplace = True)
+# data['question1'], data['question2'] = data['question1'].apply(clean_text), data['question2'].apply(clean_text)
+# print("[INFO]...Data transformation completed")
 
-#Loadind universal sentence encoder
-print("[INFO]...Loadind universal sentence encoder")
-embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder-large/5')
-# creating a method for embedding and will using method for every input layer 
-def UniversalEmbedding(doc):
-    return embed(tf.squeeze(tf.cast(doc, tf.string)))
-print("[INFO]...Pretrained weights from tensorflow hub successfully loaded")
+# #Loadind universal sentence encoder
+# print("[INFO]...Loadind universal sentence encoder")
+# embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder-large/5')
+# # creating a method for embedding and will using method for every input layer 
+# def UniversalEmbedding(doc):
+#     return embed(tf.squeeze(tf.cast(doc, tf.string)))
+# print("[INFO]...Pretrained weights from tensorflow hub successfully loaded")
+
 
 # Tf GPU memory graph
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -49,48 +51,47 @@ if gpus:
         print(e)
         
 # Building network
-model = build(UniversalEmbedding)
+model = build(verbosity=1)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print("[INFO]...Model Build Completed")
 # Preparing training & validation data
-from sklearn.model_selection import train_test_split
-x1 = data['question1']
-x2 = data['question2']
-y = data['is_duplicate']
+x1 = np.load('./data/q1_use_embeddings_404287.npy')
+x2 = np.load('./data/q1_use_embeddings_404287.npy')
+labels = data['is_duplicate']
 # Using the sklearn to split data in question1 and question2 train and test in the ration 80-20 %
-x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2, random_state=42)
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, labels, test_size=0.25, random_state=42)
 
-train_q1 = x1_train.tolist()
-train_q1 = np.array(train_q1, dtype=object)[:, np.newaxis]
-train_q2 = x2_train.tolist()
-train_q2 = np.array(train_q2, dtype=object)[:, np.newaxis]
+# train_q1 = x1_train.tolist()
+# train_q1 = np.array(train_q1, dtype=object)[:, np.newaxis]
+# train_q2 = x2_train.tolist()
+# train_q2 = np.array(train_q2, dtype=object)[:, np.newaxis]
 
-#train_labels = np.asarray(pd.get_dummies(y_train), dtype = np.int8)
-train_labels = to_categorical(y_train, num_classes=2)
+# #train_labels = np.asarray(pd.get_dummies(y_train), dtype = np.int8)
+# train_labels = to_categorical(y_train, num_classes=2)
 
-test_q1 = x1_test.tolist()
-test_q1 = np.array(test_q1, dtype=object)[:, np.newaxis]
-test_q2 = x2_test.tolist()
-test_q2 = np.array(test_q2, dtype=object)[:, np.newaxis]
+# test_q1 = x1_test.tolist()
+# test_q1 = np.array(test_q1, dtype=object)[:, np.newaxis]
+# test_q2 = x2_test.tolist()
+# test_q2 = np.array(test_q2, dtype=object)[:, np.newaxis]
 
-test_labels = to_categorical(y_test, num_classes=2)
+# test_labels = to_categorical(y_test, num_classes=2)
 
 # Callbacks list
 callbacks_list = callbacks()
 
 #Mlflow config
-mlflow_tag = {'SimilarityNet':'Dense512', 'Dataset':'Quora duplicate question'}
+mlflow_tag = {'SimilarityNet':'DenseIncremental', 'Dataset':'Quora question pair'}
 mlflow.set_tracking_uri('http://127.0.0.1:5000')
 
-with mlflow.start_run(experiment_id=2, run_name='duplicate_que', nested=True):
+with mlflow.start_run(experiment_id=2, run_name='using_embeddings', nested=True):
     mlflow.set_tags(mlflow_tag)
     with tf.Session() as session:
         K.set_session(session)
         session.run(tf.global_variables_initializer())
         session.run(tf.tables_initializer())
-        history = model.fit([train_q1, train_q2],
-                            train_labels,
-                            validation_data=([test_q1, test_q2], test_labels),
+        history = model.fit([x1_train, x2_train],
+                            y_train,
+                            validation_data=([x1_test, x2_test], y_test),
                             callbacks=callbacks_list,
                             epochs=20,
                             batch_size=512)
