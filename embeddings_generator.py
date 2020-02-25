bf298e0fda39b2be6f3ab1432a466add1dd13160
Akash Desarda <desardaakash@gmail.com>
2020-02-25 17:43:11 +0530

    incomplete

--- embeddings_generator.py
+++ embeddings_generator.py
@@ -4,3 +4,10 @@ import tensorflow as tf
 tf.compat.v1.enable_eager_execution()
 
 #Incomplete
+# #Loadind universal sentence encoder
+# print("[INFO]...Loadind universal sentence encoder")
+# embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder-large/5')
+# # creating a method for embedding and will using method for every input layer 
+# def UniversalEmbedding(doc):
+#     return embed(tf.squeeze(tf.cast(doc, tf.string)))
+# print("[INFO]...Pretrained weights from tensorflow hub successfully loaded")
