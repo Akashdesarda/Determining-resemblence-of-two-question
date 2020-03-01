58fd1ed05e9634a2f853a8311d3758e7bcac2d2d
Akash Desarda <desardaakash@gmail.com>
2020-03-02 00:11:07 +0530

    moving to correct location

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -3,9 +3,25 @@ warnings.filterwarnings('ignore')
 
 import re
 from typing import Dict
+import tensorflow as tf
 from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report
 from datetime import datetime
 now = datetime.now().strftime("%d-%m-%Y")
+
+def limit_gpu():
+    # Tf GPU memory graph
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print('[INFO]... ',len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
 def clean_text(doc):
     """Cleaning of text data
@@ -62,4 +78,16 @@ def visualize(history: Dict, save_plot: bool=True, save_dir: str=None):
     else:
         plt.show()
     
-
+def report(y_true, y_pred):
+    """Logging of report
+    
+    Parameters
+    ----------
+    y_true : numpy array or pandas series
+        lables of test data
+    y_pred : numpy array or pandas series
+        labels of predicted data
+    """
+    print(f"[REPORT]...Accuracy of recently trained model is: {accuracy_score(y_true, y_pred)}")
+    print("Following is detailed classification Report")
+    print(classification_report(y_true, y_pred))
